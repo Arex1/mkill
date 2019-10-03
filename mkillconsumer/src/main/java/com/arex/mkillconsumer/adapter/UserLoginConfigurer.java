fb22ab7edbf5c0be49458c664cfd296d5a6fa400
arex <1770349336@qq.com>
2019-10-03 18:16:56 +0800

    添加拦截器

---
+++ mkillconsumer/src/main/java/com/arex/mkillconsumer/adapter/UserLoginConfigurer.java
@@ -0,0 +1,15 @@
+package com.arex.mkillconsumer.adapter;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Arex
+ *
+ * @author weiwei-gu
+ * com.arex.mkillconsumer.adapter
+ * 2019/10/3
+ */
+public class UserLoginConfigurer implements WebMvcConfigurer {
+
+
+}
---
+++ mkillconsumer/src/main/java/com/arex/mkillconsumer/interceptor/UserLoginInterceptor.java
@@ -0,0 +1,11 @@
+package com.arex.mkillconsumer.interceptor;
+
+/**
+ * Arex
+ *
+ * @author weiwei-gu
+ * com.arex.mkillconsumer.interceptor
+ * 2019/10/3
+ */
+public class UserLoginInterceptor {
+}
