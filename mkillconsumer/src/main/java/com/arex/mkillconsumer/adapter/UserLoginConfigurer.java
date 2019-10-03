d67927087c71c496f9e946c812de1d77674cf1c1
arex <1770349336@qq.com>
2019-10-03 18:18:03 +0800

    添加拦截器

--- mkillconsumer/src/main/java/com/arex/mkillconsumer/adapter/UserLoginConfigurer.java
+++ mkillconsumer/src/main/java/com/arex/mkillconsumer/adapter/UserLoginConfigurer.java
@@ -1,6 +1,12 @@
 package com.arex.mkillconsumer.adapter;
 
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import com.arex.mkillconsumer.interceptor.UserLoginInterceptor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.servlet.config.annotation.*;
+
+import javax.annotation.Resource;
 
 /**
  * Arex
@@ -9,7 +15,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * com.arex.mkillconsumer.adapter
  * 2019/10/3
  */
+@Configuration
+@EnableWebMvc
+@ComponentScan
 public class UserLoginConfigurer implements WebMvcConfigurer {
 
+    @Resource
+    private UserLoginInterceptor interceptor;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login.html").setViewName("login");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor).addPathPatterns("/**")
+                .excludePathPatterns("/**", "/login")
+                .excludePathPatterns("/static/**");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //第一个方法设置访问路径前缀，第二个方法设置资源路径
+        registry.addResourceHandler("/resources/**", "/static/**")
+                .addResourceLocations("classpath:/resources/", "classpath:/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+    }
 
 }
--- mkillconsumer/src/main/java/com/arex/mkillconsumer/interceptor/UserLoginInterceptor.java
+++ mkillconsumer/src/main/java/com/arex/mkillconsumer/interceptor/UserLoginInterceptor.java
@@ -1,5 +1,17 @@
 package com.arex.mkillconsumer.interceptor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
 /**
  * Arex
  *
@@ -7,5 +19,48 @@ package com.arex.mkillconsumer.interceptor;
  * com.arex.mkillconsumer.interceptor
  * 2019/10/3
  */
-public class UserLoginInterceptor {
+@Component
+public class UserLoginInterceptor implements HandlerInterceptor {
+    protected static final Logger logger = LoggerFactory.getLogger(UserLoginInterceptor.class);
+
+    /**
+     * 在请求处理之前调用,只有返回true才会执行请求
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+
+        //得到session
+        HttpSession session = httpServletRequest.getSession(true);
+
+        //得到对象
+        Object admin = session.getAttribute("6666");
+
+        //判断对象是否存在
+        if (StringUtils.isEmpty(admin)) {
+            //不存在则跳转到登录页
+            logger.info("进入登录页面");
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/login");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 试图渲染之后执行
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
+
+    }
+
+    /**
+     * 在请求处理之后,视图渲染之前执行
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+
 }
+
